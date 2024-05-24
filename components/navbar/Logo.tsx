a23a1f6d84765adf25f9a0eea0c7c7ddc76c7325
Satyam Singh <76854874+PandaTryingCoding@users.noreply.github.com>
2024-05-25 01:03:05 +0530

    Added skeletal styles and designs to the Nav Search and the Icon

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { LuWarehouse } from "react-icons/lu";
+import Link from "next/link";
+import { Button } from "../ui/button";
 
 function Logo() {
-  return <div>Logo</div>;
+  return (
+    <Button size='icon' asChild>
+      <Link href='/'>
+        <LuWarehouse className='w-6 h-6' />
+      </Link>
+    </Button>
+  );
 }
 
 export default Logo;
