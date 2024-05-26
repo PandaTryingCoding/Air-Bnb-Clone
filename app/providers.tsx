ad003615e018d215fa957c458ae18b34cd82d6eb
Satyam Singh <76854874+PandaTryingCoding@users.noreply.github.com>
2024-05-27 04:38:55 +0530

    Made the signout link component initial skeletal

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,13 @@
 "Use Client";
 
+import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "./theme-provider";
 import React from "react";
 
 function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Toaster />
       <ThemeProvider
         attribute='class'
         defaultTheme='system'
