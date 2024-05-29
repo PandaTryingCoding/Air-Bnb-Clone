ea23ee79e662def82fec7a8391888cfe05ae41bf
Satyam Singh <76854874+PandaTryingCoding@users.noreply.github.com>
2024-05-29 21:46:50 +0530

    Redirect user to Home page on successfull completion of create a profile

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -2,11 +2,15 @@ import SubmitButton from "@/components/form/Buttons";
 import FormContainer from "@/components/form/FormContainer";
 import FormInput from "@/components/form/FormInput";
 import { createProfileAction } from "@/utils/actions";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-function CreateProfile() {
+async function CreateProfile() {
+  const user = await currentUser();
+  if (user?.privateMetadata?.hasProfile) redirect("/");
   return (
     <section>
-      <h1 className='text-2xl f0nt-semibold mb-8 capitalize'>New User</h1>
+      <h1 className='text-2xl font-semibold mb-8 capitalize'>New User</h1>
       <div className='border p-8 rounded-md '>
         <FormContainer action={createProfileAction}>
           <div className='grid md:grid-cols-2 mt-4 gap-4'>
