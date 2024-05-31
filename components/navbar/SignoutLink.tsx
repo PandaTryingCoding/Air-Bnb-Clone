840f559846f957de154518ba5d099085729fe283
Satyam Singh <76854874+PandaTryingCoding@users.noreply.github.com>
2024-06-01 02:46:33 +0530

    Fixed the condition where after signout the user was still on the page and not getting back to the homepage

--- components/navbar/SignoutLink.tsx
+++ components/navbar/SignoutLink.tsx
@@ -4,14 +4,17 @@ import React from "react";
 
 import { SignOutButton } from "@clerk/nextjs";
 import { useToast } from "../ui/use-toast";
+import { useRouter } from "next/navigation";
 
 function SignoutLink() {
+  const router = useRouter();
   const { toast } = useToast();
   const handleLogout = () => {
     toast({ description: "You've Successfully Been Signed Out!" });
+    router.push("/");
   };
   return (
-    <SignOutButton redirectUrl='/'>
+    <SignOutButton>
       <button className='w-full text-left' onClick={handleLogout}>
         Logout
       </button>
