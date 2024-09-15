1d8d688086f9e092aac7a4a5ebd4d3cfe4ef181f
unknown <satyamjsr999@gmail.com>
2024-09-15 12:30:05 +0530

    Created Nav Search and Property list search component in Home Page

--- middleware.ts
+++ middleware.ts
@@ -16,3 +16,18 @@ export default clerkMiddleware((auth, req) => {
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+// import { NextResponse } from "next/server";
+// const isPublicRoute = createRouteMatcher(["/", "/properties(.*)"]);
+// const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
+// export default clerkMiddleware(async (auth, req) => {
+//   const isAdminUser = auth().userId === process.env.ADMIN_USER_ID;
+//   if (isAdminRoute(req) && !isAdminUser) {
+//     return NextResponse.redirect(new URL("/", req.url));
+//   }
+//   if (!isPublicRoute(req)) auth().protect();
+// });
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
