6b4d2ced7917e92c6c5cb63a0aa931a1f27e449a
unknown <satyamjsr999@gmail.com>
2024-10-02 12:28:13 +0530

    Added the Favourites page and the functionality to add and remove a property from the favourites page

--- app/favourites/page.tsx
+++ app/favourites/page.tsx
@@ -1,5 +1,14 @@
-function FavouritesPage() {
-  return <div className='text-3xl'>Favourites Page</div>;
-}
+import EmptyList from "@/components/home/EmptyList";
+import PropertiesList from "@/components/home/PropertiesList";
+import { fetchFavorites } from "@/utils/actions";
 
-export default FavouritesPage;
+async function FavoritesPage() {
+  const favorites = await fetchFavorites();
+
+  if (favorites.length === 0) {
+    return <EmptyList />;
+  }
+
+  return <PropertiesList properties={favorites} />;
+}
+export default FavoritesPage;
