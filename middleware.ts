3c3853f31de00555705d823d6753cd1abca8e910
PandaTryingCoding <satyamjsr999@gmail.com>
2025-03-09 07:37:33 +0530

    Set up the middle ware for admin where they get access to the admin page and also set logic to display the admin page link in the dropdown

--- middleware.ts
+++ middleware.ts
@@ -1,33 +1,18 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher([
-  "/bookings(.*)",
-  "/checkout(.*)",
-  "/favourites(.*)",
-  "/profile(.*)",
-  "/rentals(.*)",
-  "/reviews(.*)",
-]);
+const isPublicRoute = createRouteMatcher(["/", "/properties(.*)"]);
+const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+export default clerkMiddleware(async (auth, req) => {
+  console.log(auth().userId);
+  const isAdminUser = auth().userId === process.env.ADMIN_USER_ID;
+  if (isAdminRoute(req) && !isAdminUser) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+  if (!isPublicRoute(req)) auth().protect();
 });
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
-
-// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-// import { NextResponse } from "next/server";
-// const isPublicRoute = createRouteMatcher(["/", "/properties(.*)"]);
-// const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
-// export default clerkMiddleware(async (auth, req) => {
-//   const isAdminUser = auth().userId === process.env.ADMIN_USER_ID;
-//   if (isAdminRoute(req) && !isAdminUser) {
-//     return NextResponse.redirect(new URL("/", req.url));
-//   }
-//   if (!isPublicRoute(req)) auth().protect();
-// });
-// export const config = {
-//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
